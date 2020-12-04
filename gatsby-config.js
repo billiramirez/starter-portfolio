c8c52782db0519ef5c34e8311acad3cb1bec461a
Billi Ramirez <ramirezbilly42@outlook.com>
2020-12-04 14:19:58 -0600

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,19 +6,10 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GOOGLE_MEASUREMENT_ID,
+        trackingId: process.env.GOOGLE_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'billiramirez.dev',
+        head: true,
+        anonymize: true,
       },
     },
     `gatsby-plugin-sass`,
