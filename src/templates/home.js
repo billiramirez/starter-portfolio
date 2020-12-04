4b3f2cf7461059a0831fd841f38db9d79a280e92
Billi Ramirez <ramirezbilly42@outlook.com>
2020-12-04 10:35:44 -0600

    should work

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
 import App from '../components/App';
-import { headData } from '../mock/data';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
-export default () => {
-  const { title, lang, description } = headData;
+const Home = ({ pageContext }) => {
+  const { title, lang, description } = pageContext.mockData.headData;
 
   return (
     <>
@@ -16,7 +17,15 @@ export default () => {
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
-      <App />
+      <App data={pageContext.mockData} />
     </>
   );
 };
+
+Home.propTypes = {
+  pageContext: PropTypes.shape({
+    mockData: PropTypes.object,
+  }),
+};
+
+export default Home;
